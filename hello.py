99a914562484c72b8a7a04489b2493fdefc024c3
sunil kumar rao s <sunilkumarraos007@gmail.com>
2026-05-20 13:16:54 +0530

    day 1 of the AI journey

---
+++ hello.py
@@ -0,0 +1,13 @@
+my_name = "sunil kumar rao s"
+my_goal = "Goal towards AI-Engineer"
+day_number = 1
+
+print(my_name,my_goal)
+print(f"Today is Day {day_number} of my AI sprint")
+
+# A simple list — like EV stations
+ev_stations = ["Koramangala", "Indiranagar", "Whitefield", "HSR Layout", "BTM Layout"]
+for station in ev_stations:
+    print(station)
+
+print("showing up is the hardest part")
